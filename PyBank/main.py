d71992dcbd4c58ed4e6d9095cee0112e9005c5fc
sydneysteele03 <126841347+sydneysteele03@users.noreply.github.com>
2023-04-04 22:48:06 -0700

    done with codes

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,12 +3,12 @@ import os, csv, math
 budget_csv = os.path.join("/Users/sydneysteele/Documents/python-challenge/PyBank/Resources/budget_data.csv")
 total = 0 
 total_months = 0 
-greatest_inc = 0 
-greatest_dec = 0 
+previous_mo = 0 
 poschanges = []
+pos_months = []
+neg_months = []
 negchanges = []
-prevloss = 0
-prevgain = 0
+
 
 # read in the csv file 
 with open(budget_csv, 'r') as csvfile:
@@ -18,39 +18,30 @@ with open(budget_csv, 'r') as csvfile:
     for row in csvreader:
         total = total + int(row[1])
         total_months = total_months + 1
-        
-        if int(row[1]) > greatest_inc:
-            poschanges.append(int(row[1])-int(prevgain))
-            prevgain = row[1]
 
-        if int(row[1]) > greatest_dec:
-            negchanges.append(int(row[1]) - int(prevloss))
-            prevloss= row[1]
-
-
-        #find greatest increase in profits 
-        #if int(row[1]) > int(greatest_inc):
-         #   greatest_inc = row[1]
-          #  inc_mo = row[0]
-        
-        #find greatest decrease in profits 
-       # if int(row[1]) < int(greatest_dec):
-        #    greatest_dec = row[1]
-           # dec_mo = row[0]
-        
-    #calculate average change... need help on this! 
-    average = ((sum(poschanges)/len(poschanges)) + (sum(negchanges)/len(negchanges)))/2
-print(poschanges)
+        #greatest increases 
+        if (int(row[1])-previous_mo) > 0: 
+            poschanges.append(int(row[1])-previous_mo)
+            pos_months.append(row[0])
+            previous_mo = int(row[1])
+            
+            
+        #greatest decreases 
+        if (int(row[1])-previous_mo) < 0:
+            negchanges.append(int(row[1])-previous_mo)
+            neg_months.append(row[0])
+            previous_mo = int(row[1])       
+    
+    #remove first month's value to accurately calculate the monthly average change 
+    average = (sum(poschanges[1:len(poschanges)]) + sum(negchanges))/(len(poschanges)+len(negchanges)-1)
 
 # display results 
-print(f'------------------------------')
 print(f'Financial Analysis')
 print(f'------------------------------')
 print(f'Total Months:'+ str(total_months))
 print(f'Total: $'+str(total))
 print(f'Average Change: $'+str("{:.2f}".format(average)))
-print(f'Greatest Increase in Profits: +inc_mo'+' '+str(prevgain.max()))
-print(f'Greatest Decrease in Profits: +dec_mo'+' '+str(prevloss.max()))
-
+print(f'Greatest Increase in Profits: ' + pos_months[poschanges.index(max(poschanges))] +' ($' + str(max(poschanges))+')')
+print(f'Greatest Decrease in Profits: ' + neg_months[negchanges.index(min(negchanges))] +' ($' + str(min(negchanges))+')')
 
 
